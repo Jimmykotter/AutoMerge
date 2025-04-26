51af89178bcbe4b352145f77d3339965bce91284
Jimmykotter <jimmykotter@gmail.com>
2025-04-26 11:57:52 -0500

    update start command

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -6,3 +6,5 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 
 export default mongoose.connection;
+
+// mongodb+srv://jimmykotter:n3uLqumcvvaorrjh@cluster0.agmqava.mongodb.net/Automerge?retryWrites=true&w=majority&appName=Cluster0
